24e24368118e7e2eeab6e67bc3bd292365cbf10e
deb0890 <deborahf1@hotmail.com>
2021-08-16 13:41:07 +0100

    create score model setup for get all route

---
+++ server/models/Scores.js
@@ -0,0 +1,27 @@
+const { init } = require ('../dbConfig')
+const { ObjectId } = require('mongodb')
+
+class Score {
+    constructor(data){
+    this.id = data.id
+    this.username = data.username
+    this.category = data.category
+    this.difficulty = data.difficulty
+    this.score = data.score
+}
+
+    static get all() {
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init()
+                const scoreData = await db.collection('scores').find().toArray()
+                const scores = scoreData.map(s => new Score({ ...s, id: data.id }))
+                resolve(scores);
+            } catch (err) {
+                reject("Error getting scores")
+            }
+        })
+    }
+}
+
+module.exports = Score;
