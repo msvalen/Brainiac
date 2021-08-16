0f6ca6d56ed56423e5ab7977b95a3b6f353abade
deb0890 <deborahf1@hotmail.com>
2021-08-16 13:43:15 +0100

    connect controller to server file

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
-
+const scoreRoutes = require('./controllers/scores')
+server.use('/', scoreRoutes)
 
 // Root
 server.get('/', (req, res) => res.send('Hello, client!'))
