1416b3ea438fa0048135aa7a9a41b20177aa2a89
deb0890 <deborahf1@hotmail.com>
2021-08-16 14:48:16 +0100

    add post route to controller

--- server/controllers/scores.js
+++ server/controllers/scores.js
@@ -8,10 +8,20 @@ const Score = require('../models/Scores')
 router.get('/', async (req, res) => {
     try {
         const scores = await Score.all
-        res.json({scores})
+        res.status(200).json({scores});
     } catch(err) {
         res.status(500).json({err})
     }
 })
+
+// Create score route
+router.post('/', async (req, res) => {
+    try {
+        const score = await Score.create(req.body.score)
+        res.status(201).json(score);
+    } catch (err) {
+        res.status(500).send({ err });
+    }
+});
 
 module.exports = router;
