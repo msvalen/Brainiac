e62c530368a42428d9a7b58932a487c8fb4f56dc
deb0890 <deborahf1@hotmail.com>
2021-08-17 11:44:54 +0100

    create quiz summary page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
 export { default as NotFound} from './NotFound';
 export { default as Home } from './Home';
 export { default as QuizPage } from './Quiz';
+export { default as QuizSummary } from './QuizSummary';
