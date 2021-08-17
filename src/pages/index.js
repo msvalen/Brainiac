7c2a2da6dc9dd9903b4373d980cf38fbacda18c5
deb0890 <deborahf1@hotmail.com>
2021-08-17 14:31:26 +0100

    exporting highscores function to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { default as NotFound} from './NotFound';
 export { default as Home } from './Home';
 export { default as QuizPage } from './Quiz';
 export { default as QuizSummary } from './QuizSummary';
+export { default as HighScores } from './HighScores'; 
