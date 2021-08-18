8f0c0fdd54fadb77016289919b74e2d4c81e0c46
deb0890 <deborahf1@hotmail.com>
2021-08-18 12:48:00 +0100

    create function to change question, pass as props

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -2,12 +2,15 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
+import { Question } from '../../components';
 
 const QuizPage = () =>{
     const [ allQuestions, setAllQuestions ] = useState([]);
+    const [ actualQuestion, setActualQuestion ] = useState(0);
     const { level } = useParams();
     const quizData = useSelector(state => state.settings)
     const categoryData = useSelector(state => state.categories)
+
 
     useEffect(() => {
         async function fullDataSet() {
@@ -25,12 +28,25 @@ const QuizPage = () =>{
         return () => clearInterval(stream)
     }, [])
 
+    useEffect(() => {
+       
+    }, [actualQuestion])
+
+    const changeQuestion = (answer) => {
+        if (allQuestions[actualQuestion].correct_answer == answer) {
+            
+            dispatch(localScores())
+        } 
+        setActualQuestion(prev => prev+1)
+    }
+
     return (
         <>
-            <h1>The Topic is: {quizData[0]}</h1>
+            { allQuestions && <Question question={allQuestions[actualQuestion]} selected={changeQuestion} /> }
+            {/* <h1>The Topic is: {quizData[0]}</h1>
             <h2>The users are: {quizData[1].map((x,i)=> <h3 key={i}>{x}</h3> )}</h2>
             <h2>The difficulty is: {quizData[2]}</h2>
-            <p>{allQuestions && console.log(allQuestions)}</p>
+            <p>{allQuestions && console.log(allQuestions)}</p> */}
         </>
     )
 }
