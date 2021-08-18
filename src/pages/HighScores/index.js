bba1af9a39e6310d49640f5d1057451bc619f944
deb0890 <deborahf1@hotmail.com>
2021-08-18 10:05:16 +0100

    filter data from database

--- src/pages/HighScores/index.js
+++ src/pages/HighScores/index.js
@@ -1,41 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchCategories } from '../../action';
-import { useState, useEffect } from 'react';
+import { fetchCategories} from '../../action';
 import axios from 'axios';
 
 
 const HighScores = () => {
     const [score, setScore] = useState();
-    const [ category, setCategory ] = useState('Animals');
-    const [ difficulty, setDifficulty ] = useState('easy')
-
-    const dispatch = useDispatch();
-
+    const [ category, setCategory ] = useState('');
+    const [ difficulty, setDifficulty ] = useState('')
+    const [allScores, setAllScores] = useState();
     
+    const dispatch = useDispatch();
+    const data1 = useSelector(state => state.categories);
+    const error = useSelector(state => state.error)
+
     useEffect(async () => {
         try {
             await dispatch(fetchCategories());
+            let { data } = await axios.get('https://brainiac-quiz.netlify.app/.netlify/functions/api')
+            setAllScores(data.scores)
         } catch (err) {
             console.log(err.message);
         }
     }, []);
     
-    useEffect(async () => {
-        try {
-            let { data } = await axios.get('https://brainiac-quiz.netlify.app/.netlify/functions/api')
-            setScore(data.scores)
-        } catch(err) {
-            console.log(err.message)
-        }
-    }, [])
-    
-    const data1 = useSelector(state => state.categories);
-    const error = useSelector(state => state.error)
-    
+    const sortScores = (a, b) => {
+        return b.score - a.score
+    }
+
+    useEffect(() => {
+        async function getScores() {
+            try {
+                const filteredScoresData = await allScores.filter(d => d.category == category && d.difficulty == difficulty)
+                console.log(filteredScoresData)
+                filteredScoresData.sort(sortScores)
+                setScore(filteredScoresData)
+            } catch(err) {
+                console.log(err.message)
+            }
+        } 
+        getScores()
+    }, [category, difficulty])
+
     const renderRows = () => {
         return score.map((s, i ) => <tr key={i}><td>{i+1}</td><td>{s.username}</td><td>progress bar</td><td>{s.score}</td></tr>)
       }
+
+    
 
 
     return (
@@ -49,9 +60,9 @@ const HighScores = () => {
             <select name="topic" form="inputParameters" id="topic" onChange={(e) => setCategory(e.target.value)}>
                 {data1 && data1.map((x,i) => <option key={i}>{x.category}</option>)}
             </select>
+            {/* <button onClick={handleGetScores}>Get Scores</button> */}
             <label htmlFor="difficulty"></label>
             <select name="difficulty" form="inputParameters" id="difficulty" onChange={(e)=> setDifficulty(e.target.value)}>
-                {/* <option value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>Easy</option> */}
                 <option value="easy">Easy</option>
                 <option value="medium">Medium</option>
                 <option value="hard">Hard</option>
