cef6234ac28ac81a2d8fc4a1d974549a5264d0b9
Scott <scott-a-ryan@hotmail.co.uk>
2021-08-18 14:58:17 +0100

    boiler plate testing & suggestions

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import mainReducer from '../reducers/index';
+
+jest.mock('axios')
+axios.get.mockResolvedValue({ data: { message: [] }})
+
+const TestProviders = ({ initState }) => {
+    initState ||= {categories: [], settings: [], scores: [], loading: false }
+    const testStore = createStore(() => mainReducer(initState, { type: '@@INIT' }), applyMiddleware(thunk))
+
+    return ({ children }) => (
+        <Provider store={testStore}>
+            { children }
+        </Provider>
+    )
+}
+
+const renderWithReduxProvider = (ui, options={}) => {
+    let TestWrapper = TestProviders(options)
+    render(ui, {wrapper: TestWrapper, ...options})
+}
 
 global.React = React;
-global.render = render;
+global.renderWithReduxProvider = renderWithReduxProvider;
 global.userEvent = userEvent;
