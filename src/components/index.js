75ab9d80cb54f6f466de1f0773ea313a3dc415e5
deb0890 <deborahf1@hotmail.com>
2021-08-20 00:15:03 +0100

    add reward component path to index

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as AddUser } from './AddUser';
 export { default as Question } from './Question';
 export { default as Timebar } from './Timebar'
+export { default as Stars } from './Stars'
