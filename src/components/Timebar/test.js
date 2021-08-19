5773c963d7e71d0c351a30da0e440f372d7ef055
keki94 <quequisegarra@gmail.com>
2021-08-19 14:18:54 +0100

    add Timebar test 100%branch

--- src/components/Timebar/test.js
+++ src/components/Timebar/test.js
@@ -1,20 +1,16 @@
-import { render } from 'react-dom'
-import {screen, waitFor} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import axios from 'axios'
-import App from '../../App'
+import Timebar from '.';
 
-describe('Name for test suite', ()=> {
-    beforeEach(() => {
-        renderWithReduxProvider(<App />) // , { initState } this might change, may also want to have it render a different init state based on the test
+describe('Timebar', ()=> {
+    test('it calls a handleClick prop when time finish', () => {
+        const stubHandleClick = jest.fn();
+        renderWithReduxProvider(<Timebar duration={1000} endAction={stubHandleClick} />)
+        setTimeout(() => {
+            expect(stubHandleClick).toHaveBeenCalledTimes(1);
+          }, 1005);
     })
-
-    test('What you want to test', () =>{
-        // expect().toBeInTheDocument();
-        // expect().toHaveBeenCalledTimes(1);
-        // expect().toBe();
-        // expect().toContain();
-        // expect().toEqual();
-        // expect().toBeInstanceOf();
+    test('it doesn\'t calls handleClick if the time hasn\'t finished', () => {
+        const stubHandleClick = jest.fn();
+        renderWithReduxProvider(<Timebar duration={1000} endAction={stubHandleClick} />)
+        expect(stubHandleClick).toHaveBeenCalledTimes(0);
     })
 })
