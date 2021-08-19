73f320a1d10fb3621cf925fb3432ef1d3dc4aac6
Scott <scott-a-ryan@hotmail.co.uk>
2021-08-19 17:08:16 +0100

    Home testing and coverage for package.json

--- src/pages/Home/test.js
+++ src/pages/Home/test.js
@@ -1,27 +1,53 @@
-import { render } from 'react-dom'
-import {screen, waitFor} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import axios from 'axios'
 import App from '../../App'
+import { Home } from '../../pages'
 
-describe('Name for test suite', ()=> {
+describe('Testing Home page', ()=> {
     beforeEach(() => {
         renderWithReduxProvider(<App />) // , { initState } this might change, may also want to have it render a different init state based on the test
     })
 
-    // describe('Home page', () => {
-    //     test('renders with correct title', () => {
-    //         const heading = screen.getByRole('heading')
-    //         expect(heading.textContent).toContain('Brainiac')
-    //     })
-    // })
+    describe('Home page', () => {
+        test('renders with correct title', () => {
+            const heading = screen.getByRole('heading', {level:1})
+            expect(heading.textContent).toContain('Brainiac')
+        })
 
-    test('What you want to test', () =>{
+        test('renders with form', () => {
+            const form = screen.getByRole('form', {name: "quizSettingsForm"})
+            expect(form).toBeInTheDocument();
+        })
+
+        // test('it pushes user to nex page on click', () => {
+        //     let locationInput = screen.getByLabelText('Location');
+        //     userEvent.type(locationInput, "Hong Kong{enter}")
+        //     expect(getResultMock).toHaveBeenNthCalledWith(1, 'Hong Kong');
+        // })
+
+        test('renders with team names', () => {
+            const heading = screen.getByRole('heading', {level:2})
+            expect(heading.textContent).toContain('Deborah, Monica & Scott')
+        })
+        
+    })
+
+    // test('What you want to test', () =>{
     //     expect().toBeInTheDocument();
     //     expect().toHaveBeenCalledTimes(1);
     //     expect().toBe();
     //     expect().toContain();
     //     expect().toEqual();
     //     expect().toBeInstanceOf();
+    // })
+})
+
+describe('Testing connective functions on Home page', ()=> {
+    test('it calls handleAddUser on click', () => {
+        const stubHandleClick = jest.fn();
+        renderWithReduxProvider(<Home handleClick={stubHandleClick}/>)
+        let addUsrBtn = screen.getByRole('button', { name: 'Add-User-Page' });
+        userEvent.click(addUsrBtn)
+        expect(stubHandleClick).toHaveBeenCalledTimes(0);
     })
 })
