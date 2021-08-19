8f91a48698b827400f602ef9e44ed2953f5ad46a
keki94 <quequisegarra@gmail.com>
2021-08-19 17:04:52 +0100

    add style

--- src/layout/Modal/test.js
+++ src/layout/Modal/test.js
@@ -1,20 +1,11 @@
-import { render } from 'react-dom'
-import {screen, waitFor} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import axios from 'axios'
-import App from '../../App'
+import Modal from '.';
 
-describe('Name for test suite', ()=> {
-    beforeEach(() => {
-        renderWithReduxProvider(<App />) // , { initState } this might change, may also want to have it render a different init state based on the test
-    })
-
-    test('What you want to test', () =>{
-        // expect().toBeInTheDocument();
-        // expect().toHaveBeenCalledTimes(1);
-        // expect().toBe();
-        // expect().toContain();
-        // expect().toEqual();
-        // expect().toBeInstanceOf();
+describe('Modal', ()=> {
+    
+    test('Itcheck if renders a div whose class is userModal', () => {      
+        const stubHandleClick = jest.fn();
+        renderWithReduxProvider(<Modal getResults={stubHandleClick} show={stubHandleClick} />)
+        let modal = document.querySelector('.userModal');
+        expect(modal).toBeInTheDocument();
     })
 })
