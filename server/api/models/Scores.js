5ef469a1c04d5e4de372f09aed09d2109a451642
Scott <scott-a-ryan@hotmail.co.uk>
2021-08-19 10:07:36 +0100

    updated server to accept many data entries

--- server/api/models/Scores.js
+++ server/api/models/Scores.js
@@ -23,27 +23,12 @@ class Score {
         })
     }
 
-    // static create(score){
-    //     return new Promise (async (resolve, reject) => {
-    //         try {
-    //             const db = await init();
-    //             let scoreData = await db.collection('scores').insertOne(score)
-    //             let newScore = new Score({...score, id:scoreData.insertedId});
-    //             resolve (newScore);
-    //         } catch (err) {
-    //             reject('Error adding new score');
-    //         }
-    //     });
-    // }
-    // insert Many
     static create(scores){
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init();
-                let scoresData = await db.collection('scores').insertMany(scores)
-                console.log(scoresData);
-                let newScores = scoresData.map((s,i)=>new Score({...scores[i], id:s.insertedId}));
-                resolve (newScores);
+                await db.collection('scores').insertMany(scores)
+                resolve ('Data Saved to Database');
             } catch (err) {
                 reject('Error adding new scores');
             }
